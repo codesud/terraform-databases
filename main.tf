2e1c1ce851e8bc9d8f5619c0fdb81362b24b3fa2
Sudeep S <sudeepdirect@gmail.com>
2022-11-09 17:17:19 +0530

    Updated tf files

---
+++ main.tf
@@ -0,0 +1,44 @@
+# module "vpc" {
+#   source = "git::https://github.com/codesud/tf-module-vpc.git
+# }
+
+module "mongodb" {
+  source           = "./vendor/modules/mongodb"
+  ENV              = var.ENV
+  WORKSPATION_IP   = var.WORKSPATION_IP
+  DOCDB_PORT       = var.DOCDB_PORT 
+  DOCDB_INSTACE_CLASS = var.DOCDB_INSTACE_CLASS
+  DOCDB_INSTACE_COUNT = var.DOCDB_INSTACE_COUNT
+}
+
+module "redis" {
+  source           = "./vendor/modules/redis"
+  ENV              = var.ENV
+  ELASTICCCACHE_NODE_TYPE      = var.ELASTICCCACHE_NODE_TYPE 
+  ELASTICCCACHE_NODE_COUNT     = var.ELASTICCCACHE_NODE_COUNT 
+  ELASTICCCACHE_PORT           = var.ELASTICCCACHE_PORT
+  ELASTICCCACHE_ENGINE_VERSION = var.ELASTICCCACHE_ENGINE_VERSION
+}
+
+module "mysql" {
+  source           = "./vendor/modules/mysql"
+  ENV              = var.ENV
+  WORKSPATION_IP   = var.WORKSPATION_IP
+  RDS_MYSQL_PORT   = var.RDS_MYSQL_PORT 
+  RDS_MYSQL_CAPACITY = var.RDS_MYSQL_CAPACITY 
+  RDS_ENGINE_VERSION = var.RDS_ENGINE_VERSION 
+  RDS_INDTSNCE_TYPE  = var.RDS_INDTSNCE_TYPE
+
+}
+
+module "rabbitmq" {
+  source           = "./vendor/modules/rabbitmq"
+  ENV              = var.ENV
+  WORKSPATION_IP   = var.WORKSPATION_IP
+  RABBITMQ_PORT    = var.RABBITMQ_PORT 
+  RABBITMQ_INSTANCE_TYPE = var.RABBITMQ_INSTANCE_TYPE
+
+}
+
+# You cannot parameterise the source section of the terraform module : Branc name cannot be parameterised. 
+# That's the challenge with terraform : Terrafile from coretech
